5a46a4a4b5aa8afb66362ed9dfbcfa4fa4e47710
kant1 <mileia418@gmail.com>
2025-07-01 09:27:16 +0000

    message + login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Routes publiques
+Route::post('/login', [AuthController::class, 'login']);
+
+// Routes protégées par authentification
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'me']);
+    Route::get('/check', [AuthController::class, 'check']);
+    
+    // Messages
+    Route::get('/conversations', [MessageController::class, 'getConversations']);
+    Route::post('/conversations', [MessageController::class, 'createConversation']);
+    Route::get('/conversations/users', [MessageController::class, 'getAvailableUsers']);
+    Route::get('/conversations/{groupId}/members', [MessageController::class, 'getGroupMembers']);
+    Route::post('/conversations/{groupId}/members', [MessageController::class, 'addGroupMembers']);
+    Route::get('/messages/{groupId}', [MessageController::class, 'getMessages']);
+    Route::post('/messages/{groupId}', [MessageController::class, 'sendMessage']);
+    Route::post('/conversations/{groupId}/mark-read', [MessageController::class, 'markAsRead']);
+    
+    // Nouvelles routes
+    Route::post('/messages/{messageId}/reactions', [MessageController::class, 'addReaction']);
+    Route::get('/files/{fichierId}', [MessageController::class, 'downloadFile']);
 });
